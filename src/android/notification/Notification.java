ae82b4d140ebbe8d389901e97df55f07bebda307
Filavision <info@filavision.de>
2024-10-15 13:59:19 +0200

    Update Notification.java

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -219,12 +219,14 @@ public final class Notification {
             if (!date.after(new Date()) && trigger(intent, receiver))
                 continue;
 
-            int flags = FLAG_UPDATE_CURRENT;
+            PendingIntent pi = null;
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                flags = PendingIntent.FLAG_MUTABLE | FLAG_UPDATE_CURRENT;
+                    pi = PendingIntent.getBroadcast(
+                        context, 0, intent, PendingIntent.FLAG_IMMUTABLE | FLAG_CANCEL_CURRENT);
+            } else {
+                    pi = PendingIntent.getBroadcast(
+                        context, 0, intent, FLAG_CANCEL_CURRENT);
             }
-            
-            PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, flags);
 
             try {
                 switch (options.getPrio()) {
@@ -326,12 +328,14 @@ public final class Notification {
         for (String action : actions) {
             Intent intent = new Intent(action);
 
-            int flags = FLAG_CANCEL_CURRENT;
+            PendingIntent pi = null;
             if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-                flags = PendingIntent.FLAG_MUTABLE | FLAG_CANCEL_CURRENT;
+                pi = PendingIntent.getBroadcast(
+                    context, 0, intent, PendingIntent.FLAG_IMMUTABLE  );
+            } else {
+                pi = PendingIntent.getBroadcast(
+                    context, 0, intent, 0);
             }
-
-            PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, flags);
 
             if (pi != null) {
                 getAlarmMgr().cancel(pi);
